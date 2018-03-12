35b7a05c86ca16ea8d133d86176d3230a9451216
Aaron Duke <dukuru@gmail.com>
2018-03-12 18:17:24 -0400

    remove django configurations dependency.

--- bgdb/config/local.py
+++ bgdb/config/local.py
@@ -1,26 +1,20 @@
-import os
-from .common import Common
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from .common import *
 
+DEBUG = True
+# Testing
+INSTALLED_APPS += ('django_nose', 'django_extensions')
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = [
+    BASE_DIR,
+    '-s',
+    '--nologcapture',
+    '--with-coverage',
+    '--with-progressive',
+    '--cover-package=bgdb'
+]
 
-class Local(Common):
-    DEBUG = True
-
-    # Testing
-    INSTALLED_APPS = Common.INSTALLED_APPS
-    INSTALLED_APPS += ('django_nose', 'django_extensions')
-    TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-    NOSE_ARGS = [
-        BASE_DIR,
-        '-s',
-        '--nologcapture',
-        '--with-coverage',
-        '--with-progressive',
-        '--cover-package=bgdb'
-    ]
-
-    # Mail
-    EMAIL_HOST = 'localhost'
-    EMAIL_PORT = 1025
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    NOTEBOOK_ARGUMENTS = ['--ip', '0.0.0.0', '--no-browser', '--allow-root']
+# Mail
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+NOTEBOOK_ARGUMENTS = ['--ip', '0.0.0.0', '--no-browser', '--allow-root']
--- manage.py
+++ manage.py
@@ -3,23 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bgdb.config")
-    os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bgdb.config.local")
     os.environ.setdefault("DJANGO_SECRET_KEY", "433ashd8a07$&SH$&*)@HF")
+    from django.core.management import execute_from_command_line
 
-    try:
-        from configurations.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django  # noqa
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
     execute_from_command_line(sys.argv)
