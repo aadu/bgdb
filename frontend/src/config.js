6e5d4e26cad52966a196402aa96998636d01dfde
Aaron Duke <dukuru@gmail.com>
2018-03-12 23:18:58 -0400

    progress

---
+++ frontend/src/config.js
@@ -0,0 +1,19 @@
+const port = process.env.API_PORT ? `:${process.env.API_PORT}` : ''
+const host = `${window.location.protocol}//${window.location.hostname}`
+const baseUrl = `${port}${host}`
+const api = `${baseUrl}/api/v1`
+
+const config = {
+  locale: 'en-US', // en-US, zh-CN
+  url: baseUrl,
+  debug: {
+    mock: true, // enable mock
+    http: false // http request log
+  },
+  api,
+  authUrl: `${api}/api-token-auth/`,
+  authUserUrl: `${api}/user-data/`
+}
+global.config = config
+
+export default config
