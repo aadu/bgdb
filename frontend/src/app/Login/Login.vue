397b3f666bb1c8236edfef7619009d8a7226b317
Aaron Duke <dukuru@gmail.com>
2018-03-13 18:36:13 -0400

    Linting fixes

--- frontend/src/app/Login/Login.vue
+++ frontend/src/app/Login/Login.vue
@@ -23,7 +23,6 @@
   </form>
 </template>
 
-
 <style>
   body{
     background: #666 !important;
@@ -33,7 +32,6 @@
 <script>
 import { validationMixin } from 'vuelidate'
 import { required, maxLength, email } from 'vuelidate/lib/validators'
-
 
 const methods = {
   onSuccess (data) {
@@ -53,45 +51,45 @@ const methods = {
 }
 
 const computed = {
-    checkboxErrors () {
-      const errors = []
-      if (!this.$v.checkbox.$dirty) return errors
-      !this.$v.checkbox.required && errors.push('You must agree to continue!')
-      return errors
-    },
-    selectErrors () {
-      const errors = []
-      if (!this.$v.select.$dirty) return errors
-      !this.$v.select.required && errors.push('Item is required')
-      return errors
-    },
-    nameErrors () {
-      const errors = []
-      if (!this.$v.name.$dirty) return errors
-      !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-      !this.$v.name.required && errors.push('Name is required.')
-      return errors
-    },
-    emailErrors () {
-      const errors = []
-      if (!this.$v.email.$dirty) return errors
-      !this.$v.email.email && errors.push('Must be valid e-mail')
-      !this.$v.email.required && errors.push('E-mail is required')
-      return errors
-    }
+  checkboxErrors () {
+    const errors = []
+    if (!this.$v.checkbox.$dirty) return errors
+    !this.$v.checkbox.required && errors.push('You must agree to continue!')
+    return errors
+  },
+  selectErrors () {
+    const errors = []
+    if (!this.$v.select.$dirty) return errors
+    !this.$v.select.required && errors.push('Item is required')
+    return errors
+  },
+  nameErrors () {
+    const errors = []
+    if (!this.$v.name.$dirty) return errors
+    !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
+    !this.$v.name.required && errors.push('Name is required.')
+    return errors
+  },
+  emailErrors () {
+    const errors = []
+    if (!this.$v.email.$dirty) return errors
+    !this.$v.email.email && errors.push('Must be valid e-mail')
+    !this.$v.email.required && errors.push('E-mail is required')
+    return errors
+  }
 }
 
 export default {
   mixins: [validationMixin],
   validations: {
     name: { required, maxLength: maxLength(10) },
-    email: { required, email },
+    email: { required, email }
   },
   methods,
   computed,
   data: () => ({
     name: '',
-    email: '',
-  }),
+    email: ''
+  })
 }
 </script>
