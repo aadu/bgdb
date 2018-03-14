f423cd7f9a3bf3d2daa6a2f598cb8eeb4a13db08
Aaron Duke <dukuru@gmail.com>
2018-03-14 00:29:00 -0400

    Extract out LoginMenu from Toolbar

--- frontend/src/app/Layout/Toolbar.vue
+++ frontend/src/app/Layout/Toolbar.vue
@@ -5,80 +5,22 @@
     <v-toolbar-side-icon @click.stop="toggleSidebar"></v-toolbar-side-icon>
     <v-toolbar-title>BGDB</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn icon @click.stop="miniVariant = !miniVariant">
-      <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-    </v-btn>
-    <v-btn icon @click.stop="clipped = !clipped">
-      <v-icon>web</v-icon>
-    </v-btn>
-    <v-btn icon @click.stop="fixed = !fixed">
-      <v-icon>remove</v-icon>
-    </v-btn>
-    <v-menu
-        transition="slide-y-transition"
-        bottom
-      >
-      <v-btn icon slot="activator">
-        <v-icon>person</v-icon>
-      </v-btn>
-      <v-card>
-      <v-list>
-        <v-list-tile avatar>
-          <v-list-tile-avatar>
-            <img src="/static/doc-images/john.jpg" alt="John">
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>John Leider</v-list-tile-title>
-            <v-list-tile-sub-title>Founder of Vuetify.js</v-list-tile-sub-title>
-          </v-list-tile-content>
-          <v-list-tile-action>
-            <v-btn
-              icon
-              :class="fav ? 'red--text' : ''"
-              @click="fav = !fav"
-            >
-              <v-icon>favorite</v-icon>
-            </v-btn>
-          </v-list-tile-action>
-        </v-list-tile>
-      </v-list>
-        <v-divider></v-divider>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-switch v-model="message" color="purple"></v-switch>
-            </v-list-tile-action>
-            <v-list-tile-title>Enable messages</v-list-tile-title>
-          </v-list-tile>
-          <v-list-tile>
-            <v-list-tile-action>
-              <v-switch v-model="hints" color="purple"></v-switch>
-            </v-list-tile-action>
-            <v-list-tile-title>Enable hints</v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn flat @click="menu = false">Cancel</v-btn>
-          <v-btn color="primary" flat @click="menu = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-menu>
+    <LoginMenu></LoginMenu>
   </v-toolbar>
 </template>
 
 <script>
 import {mapActions, mapGetters} from 'vuex'
+import { LoginMenu } from '../Login'
 
 const name = 'toolbar'
-
+const components = {
+  LoginMenu
+}
 const methods = {
   ...mapActions([
     `toggleSidebar`
-  ]),
-  handleInput (event) {
-    console.log(event)
-  }
+  ])
 }
 
 const computed = {
@@ -86,27 +28,11 @@ const computed = {
     `sidebarVisible`
   ])
 }
+
 export default {
   name,
   methods,
   computed,
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Board Game DataBase'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-      message: '',
-      hints: '',
-      fav: ''
-    }
-  }
+  components
 }
 </script>
--- frontend/src/app/Login/index.js
+++ frontend/src/app/Login/index.js
@@ -1,2 +1,4 @@
 /* eslint-disable */
 export { default as routes } from './routes'
+export { default as LoginMenu } from './LoginMenu'
+export { default as Login } from './Login'
