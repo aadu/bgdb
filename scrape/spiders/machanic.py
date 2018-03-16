2e86736d5c16d96a91ea196923460958226216f4
Aaron Duke <dukuru@gmail.com>
2018-03-16 16:08:00 -0400

    Fix mechanic loader

--- scrape/spiders/machanic.py
+++ scrape/spiders/machanic.py
@@ -5,16 +5,16 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst, MapCompose, Join, Identity
 from scrapy.linkextractors import LinkExtractor
-from scrape.items import MechanismItem
+from scrape.items import MechanicItem
 
 
-class MechanismLoader(ItemLoader):
+class MechanicLoader(ItemLoader):
     default_output_processor = TakeFirst()
     default_input_processor = MapCompose(str.strip)
     description_out = Join()
 
 
-class MechanismSpider(CrawlSpider):
+class MechanicSpider(CrawlSpider):
     name = 'mechanic'
     allowed_domains = ['boardgamegeek.com']
     start_urls = [f'http://boardgamegeek.com/browse/{CATEGORY}/']
@@ -24,7 +24,7 @@ class MechanismSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        loader = MechanismLoader(item=MechanismItem(), response=response)
+        loader = MechanicLoader(item=MechanicItem(), response=response)
         loader.add_value('id', response.url, re=r'(\d+)')
         loader.add_value('url', response.url)
         loader.add_css('name', '.geekitem_name::text')
