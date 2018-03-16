51f294494948085b8572db1b82d1f080d5ba6e33
Aaron Duke <dukuru@gmail.com>
2018-03-16 15:19:45 -0400

    scrape.sh take args

--- scrape.sh
+++ scrape.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-DATABASE_URL="postgres://postgres:@localhost:5432/postgres" scrapy runspider scrape/spiders/subcategory.py
+DATABASE_URL="postgres://postgres:@localhost:5432/postgres" scrapy runspider scrape/spiders/$1.py
