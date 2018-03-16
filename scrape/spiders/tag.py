7b0a5b68679486b508c5d094509bd292c2a68acd
Aaron Duke <dukuru@gmail.com>
2018-03-16 16:06:19 -0400

    Fix broken tag spider

--- scrape/spiders/tag.py
+++ scrape/spiders/tag.py
@@ -5,7 +5,7 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst, MapCompose, Join, Identity
 from scrapy.linkextractors import LinkExtractor
-from scrape.items import MechanismItem
+from scrape.items import TagItem
 
 
 class TagLoader(ItemLoader):
@@ -15,7 +15,7 @@ class TagLoader(ItemLoader):
 
 
 class TagSpider(CrawlSpider):
-    name = 'subcategory'
+    name = 'tag'
     allowed_domains = ['boardgamegeek.com']
     start_urls = [f'http://boardgamegeek.com/browse/{CATEGORY}/']
 
@@ -24,7 +24,7 @@ class TagSpider(CrawlSpider):
         callback='parse_item'), )
 
     def parse_item(self, response):
-        loader = TagLoader(item=MechanismItem(), response=response)
+        loader = TagLoader(item=TagItem(), response=response)
         loader.add_value('id', response.url, re=r'(\d+)')
         loader.add_value('url', response.url)
         loader.add_css('name', '.geekitem_name::text')
