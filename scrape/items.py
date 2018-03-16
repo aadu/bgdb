24164b513d2e46bf34326c463da82d99ae07697b
Aaron Duke <dukuru@gmail.com>
2018-03-16 15:20:04 -0400

    Add new entity items

--- scrape/items.py
+++ scrape/items.py
@@ -1,7 +1,23 @@
 import scrapy
 
-from bgdb.game.serializers import GameSerializer, SubcategorySerializer
+from bgdb.game.serializers import (
+    CategorySerializer,
+    GameSerializer,
+    SubcategorySerializer,
+    MechanismSerializer,
+    TagSerializer,
+    HonorSerializer,
+    PublisherSerializer,
+    ArtistSerializer,
+    DesignerSerializer,
+)
 from .utils import DRFItem
+
+
+class CategoryItem(DRFItem):
+    class Meta:
+        serializer = CategorySerializer
+        include = ['id']
 
 
 class SubcategoryItem(DRFItem):
@@ -10,7 +26,43 @@ class SubcategoryItem(DRFItem):
         include = ['id']
 
 
+class MechanismItem(DRFItem):
+    class Meta:
+        serializer = MechanismSerializer
+        include = ['id']
+
+
 class GameItem(DRFItem):
     class Meta:
         serializer = GameSerializer
+        include = ['id']
+
+
+class TagItem(DRFItem):
+    class Meta:
+        serializer = TagSerializer
+        include = ['id']
+
+
+class HonorItem(DRFItem):
+    class Meta:
+        serializer = HonorSerializer
+        include = ['id']
+
+
+class PublisherItem(DRFItem):
+    class Meta:
+        serializer = PublisherSerializer
+        include = ['id']
+
+
+class ArtistItem(DRFItem):
+    class Meta:
+        serializer = ArtistSerializer
+        include = ['id']
+
+
+class DesignerItem(DRFItem):
+    class Meta:
+        serializer = DesignerSerializer
         include = ['id']
