049cd06de8d3c265e1cef4376ed0d7c538782005
Aaron Duke <dukuru@gmail.com>
2018-03-26 20:06:10 -0700

    Improved column select component

--- frontend/src/app/components/ColumnSelect.vue
+++ frontend/src/app/components/ColumnSelect.vue
@@ -4,24 +4,26 @@
       <v-icon>{{ icon }}</v-icon>
     </v-btn>
     <v-fade-transition mode="out-in">
-      <v-card v-if="visible" class="column-select">
-        <v-card-title>Select Columns</v-card-title>
-        <v-divider></v-divider>
-        <v-card-text>
-          <v-checkbox
-            v-for="field in fields"
-            :label="field.text"
-            :key="field.value"
-            @change="$emit('update:list', $event)"
-            :inputValue="list"
-            :value="field.value">
-            </v-checkbox>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="blue darken-1" flat @click.native="visible=false">Close</v-btn>
-        </v-card-actions>
-      </v-card>
+      <div v-if="visible" id="column-select-modal">
+        <v-card class="column-select">
+          <v-card-title>Select Columns</v-card-title>
+          <v-divider></v-divider>
+          <v-card-text>
+            <v-checkbox
+              v-for="field in fields"
+              :label="field.text"
+              :key="field.value"
+              @change="$emit('update:list', $event)"
+              :inputValue="list"
+              :value="field.value">
+              </v-checkbox>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="blue darken-1" flat @click.native="visible=false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+        </div>
     </v-fade-transition>
     </v-container>
 </template>
@@ -43,17 +45,32 @@ const props = {
   }
 }
 
+const methods = {
+  onClick (event) {
+    const el = document.getElementById('column-select-modal')
+    if (event.target === el) {
+      this.visible = false
+    }
+  }
+}
+
 export default {
   name: 'columnSelect',
+  methods,
   props,
   data () {
     return {
       visible: false
     }
+  },
+  mounted () {
+    document.addEventListener('click', this.onClick)
+  },
+  destroyed () {
+    document.removeEventListener('click', this.onClick)
   }
 }
 </script>
-
 
 
 <style scoped>
@@ -62,5 +79,22 @@ export default {
     top: 70px;
     min-width: 200px;
     max-width: 650px;
+    z-index: 2;
   }
+  #column-select-modal {
+    position: absolute;
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.05); /* Black w/ opacity */
+    -webkit-animation-name: fadeIn; /* Fade in the background */
+    -webkit-animation-duration: 0.4s;
+    animation-name: fadeIn;
+    animation-duration: 0.4s
+}
+
 </style>
