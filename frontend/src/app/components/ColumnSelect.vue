ea691fbea5b8a6bff82cd181e70f99b01e2801de
Aaron Duke <dukuru@gmail.com>
2018-03-26 22:35:22 -0700

    Simplified column select

--- frontend/src/app/components/ColumnSelect.vue
+++ frontend/src/app/components/ColumnSelect.vue
@@ -1,33 +1,35 @@
 <template>
-  <div>
-    <v-btn icon @click.stop="visible = !visible">
-      <v-icon color="grey">{{ icon }}</v-icon>
-    </v-btn>
-    <v-fade-transition mode="out-in">
-      <div v-if="visible" id="column-select-modal">
-        <v-card class="column-select">
-          <v-card-title>Select Columns</v-card-title>
-          <v-divider></v-divider>
-          <v-card-text>
-            <v-checkbox
-              v-for="field in fields"
-              :label="field.text"
-              :key="field.value"
-              @change="$emit('update:list', $event)"
-              :inputValue="list"
-              :value="field.value">
-              </v-checkbox>
-          </v-card-text>
-          <v-divider></v-divider>
-          <v-card-actions>
-            <v-btn color="blue darken-1" flat @click.native="visible=false">Close</v-btn>
-          </v-card-actions>
-        </v-card>
-        </div>
-    </v-fade-transition>
+  <div class="text-xs-center">
+    <v-menu
+      offset-x
+      :close-on-content-click="false"
+      :nudge-width="200"
+      v-model="visible"
+    >
+      <v-btn icon slot="activator">
+        <v-icon color="grey">{{ icon }}</v-icon>
+      </v-btn>
+      <v-card>
+        <v-list>
+          <v-list-tile v-for="field in fields" :key="field.value">
+            <v-list-tile-action>
+              <v-checkbox
+                @change="$emit('update:list', $event)"
+                :inputValue="list"
+                :value="field.value">
+                </v-checkbox>
+            </v-list-tile-action>
+            <v-list-tile-title>{{ field.text }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat @click="visible = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu>
   </div>
 </template>
-
 
 <script>
 const props = {
@@ -45,56 +47,13 @@ const props = {
   }
 }
 
-const methods = {
-  onClick (event) {
-    const el = document.getElementById('column-select-modal')
-    if (event.target === el) {
-      this.visible = false
-    }
-  }
-}
-
 export default {
   name: 'columnSelect',
-  methods,
   props,
   data () {
     return {
       visible: false
     }
-  },
-  mounted () {
-    document.addEventListener('click', this.onClick)
-  },
-  destroyed () {
-    document.removeEventListener('click', this.onClick)
   }
 }
 </script>
-
-
-<style scoped>
-  .column-select {
-    position: absolute;
-    top: 70px;
-    min-width: 200px;
-    max-width: 650px;
-    z-index: 2;
-  }
-  #column-select-modal {
-    position: absolute;
-    z-index: 1; /* Sit on top */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.05); /* Black w/ opacity */
-    -webkit-animation-name: fadeIn; /* Fade in the background */
-    -webkit-animation-duration: 0.4s;
-    animation-name: fadeIn;
-    animation-duration: 0.4s
-}
-
-</style>
