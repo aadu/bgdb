abcafa0d1859fcd4b185b1d638e386c758a36307
Aaron Duke <dukuru@gmail.com>
2018-03-29 10:11:45 -0700

    Split out card

---
+++ frontend/src/app/game/components/GameCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card class="game">
+    <v-card-media :src="game.image" height="400"></v-card-media>
+    <v-card-title primary-title>
+      <h1>{{ game.name }}</h1>
+      <div v-html="game.description"></div>
+    </v-card-title>
+    <v-card-text>
+      <GameStats :game="game"></GameStats>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn flat color="orange">Share</v-btn>
+      <v-btn flat color="orange">Explore</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import GameStats from './GameStats'
+
+const components = {
+  GameStats
+}
+
+const props = {
+  game: {
+    required: true,
+    type: Object
+  }
+}
+
+export default {
+  name: 'gameCard',
+  components,
+  props,
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.game {
+  background: grey%;
+}
+
+</style>
+
+
--- frontend/src/app/game/components/GameDetail.vue
+++ frontend/src/app/game/components/GameDetail.vue
@@ -3,20 +3,7 @@
     <v-container fluid grid-list-md v-if="!loading">
       <v-layout row wrap>
         <v-flex md8 d-flex>
-          <v-card class="game">
-            <v-card-media :src="game.image" height="400"></v-card-media>
-            <v-card-title primary-title>
-              <h1>{{ game.name }}</h1>
-              <div v-html="game.description"></div>
-            </v-card-title>
-            <v-card-text>
-              <GameStats :game="game"></GameStats>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn flat color="orange">Share</v-btn>
-              <v-btn flat color="orange">Explore</v-btn>
-            </v-card-actions>
-          </v-card>
+          <GameCard :game="game"></GameCard>
         </v-flex>
         <v-flex md4>
           <v-card height="100%">
@@ -59,12 +46,12 @@
 
 <script>
 import { mapActions, mapState } from 'vuex'
-import GameStats from './GameStats'
+import GameCard from './GameCard'
 import { Game } from '../models'
 const name = 'gameDetail'
 
 const components = {
-  GameStats
+  GameCard
 }
 
 const props = {
