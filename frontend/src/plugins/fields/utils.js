fa6300802f398168aa61ae079193bbbde61dd44b
Aaron Duke <dukuru@gmail.com>
2018-04-08 19:01:11 -0700

    Allow camel_seperated fields

--- frontend/src/plugins/fields/utils.js
+++ frontend/src/plugins/fields/utils.js
@@ -2,6 +2,7 @@ const normCase = (s) => {
   return s
     .replace(/([A-Z])/g, ' $1')
     .replace(/^./, str => str.toUpperCase())
+    .replace(/[-_](.)/g, str => ` ${str.slice(1).toUpperCase()}`)
 }
   // uppercase the first character
 
